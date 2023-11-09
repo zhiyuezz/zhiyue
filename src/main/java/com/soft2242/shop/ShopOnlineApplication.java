123ebec7756966ca62ab158efa0093651c13f778
zhiyuezz <3032149617@qq.com>
2023-11-09 16:24:40 +0800

    配置项目相关实体

---
+++ src/main/java/com/soft2242/shop/ShopOnlineApplication.java
@@ -0,0 +1,15 @@
+package com.soft2242.shop;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.soft2242.shop.mapper")
+public class ShopOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopOnlineApplication.class, args);
+    }
+
+}
